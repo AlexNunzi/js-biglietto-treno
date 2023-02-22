c8b53fac90e698f3afcb011e0107a37ae01f8b62
Alex Nunzi <alex.nunzi@hotmail.it>
2023-02-22 16:04:46 +0100

    Rimossi console.log() utilizzati per il debug

--- js/main.js
+++ js/main.js
@@ -11,21 +11,15 @@ if(userAge < 18){
     userTravelPrice = (userTravelBasePrice - userDiscount).toFixed(2);
     document.getElementById("discountReminder").innerHTML = "a cui viene applicato uno sconto del 20% per i minorenni."
     document.getElementById("discount").innerHTML = (userDiscount).toFixed(2);
-    console.log("minore di 18");
-    console.log(userTravelPrice);
 } else if(userAge >= 65){
     userDiscount = ((userTravelBasePrice * 40) / 100);
     userTravelPrice = (userTravelBasePrice - userDiscount).toFixed(2);
     document.getElementById("discountReminder").innerHTML = "a cui viene applicato uno sconto del 40% per i clienti senior."
     document.getElementById("discount").innerHTML = (userDiscount).toFixed(2);
-    console.log("maggiore di 65");
-    console.log(userTravelPrice);
 } else{
     userTravelPrice = userTravelBasePrice.toFixed(2);
     userDiscount = 0;
     document.getElementById("discount").innerHTML = (userDiscount).toFixed(2);
-    console.log("da 18 a 64 anni");
-    console.log(userTravelPrice);
 }
 
 document.getElementById("distance").innerHTML = userTravel;
