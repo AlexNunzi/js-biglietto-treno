03c9d3f0330adf1b297de83261ec26945680a9ab
Alex Nunzi <alex.nunzi@hotmail.it>
2023-02-22 15:59:51 +0100

    Utilizzato metodo .toFixed(2); per rappresentare solo 2 numeri dopo la virgola nei risultati delle operazioni. Migliorata esperienza utente aggiungendo informazioni aggiuntive che cambiano in base ai dati inseriti.

--- js/main.js
+++ js/main.js
@@ -8,20 +8,27 @@ var userDiscount;
 
 if(userAge < 18){
     userDiscount = ((userTravelBasePrice * 20) / 100);
-    userTravelPrice = (userTravelBasePrice - userDiscount);
+    userTravelPrice = (userTravelBasePrice - userDiscount).toFixed(2);
+    document.getElementById("discountReminder").innerHTML = "a cui viene applicato uno sconto del 20% per i minorenni."
+    document.getElementById("discount").innerHTML = (userDiscount).toFixed(2);
     console.log("minore di 18");
     console.log(userTravelPrice);
 } else if(userAge >= 65){
     userDiscount = ((userTravelBasePrice * 40) / 100);
-    userTravelPrice = (userTravelBasePrice - userDiscount);
+    userTravelPrice = (userTravelBasePrice - userDiscount).toFixed(2);
+    document.getElementById("discountReminder").innerHTML = "a cui viene applicato uno sconto del 40% per i clienti senior."
+    document.getElementById("discount").innerHTML = (userDiscount).toFixed(2);
     console.log("maggiore di 65");
     console.log(userTravelPrice);
 } else{
-    userTravelPrice = userTravelBasePrice;
+    userTravelPrice = userTravelBasePrice.toFixed(2);
+    userDiscount = 0;
+    document.getElementById("discount").innerHTML = (userDiscount).toFixed(2);
     console.log("da 18 a 64 anni");
     console.log(userTravelPrice);
 }
 
 document.getElementById("distance").innerHTML = userTravel;
 document.getElementById("age").innerHTML = userAge;
+document.getElementById("basePrice").innerHTML = userTravelBasePrice.toFixed(2);
 document.getElementById("price").innerHTML = userTravelPrice;
