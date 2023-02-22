b2b769d93e307e37a7d48f3ebabf8a0a56f21613
Alex Nunzi <alex.nunzi@hotmail.it>
2023-02-22 15:39:27 +0100

    Implementata logica di calcolo del prezzo del biglietto

--- js/main.js
+++ js/main.js
@@ -2,9 +2,26 @@ alert("Hai richiesto di calcolare il costo del biglietto per il tragitto che vuo
 const userTravel = parseInt(prompt("Quanti Km è lungo il tragitto?"));
 const userAge = parseInt(prompt("Qunti anni ha l'intestatario del biglietto?"));
 const distancePrice = 0.21;
+var userTravelBasePrice = userTravel * distancePrice;
+var userTravelPrice;
+var userDiscount;
+
+if(userAge < 18){
+    userDiscount = ((userTravelBasePrice * 20) / 100);
+    userTravelPrice = (userTravelBasePrice - userDiscount);
+    console.log("minore di 18");
+    console.log(userTravelPrice);
+} else if(userAge >= 65){
+    userDiscount = ((userTravelBasePrice * 40) / 100);
+    userTravelPrice = (userTravelBasePrice - userDiscount);
+    console.log("maggiore di 65");
+    console.log(userTravelPrice);
+} else{
+    userTravelPrice = userTravelBasePrice;
+    console.log("da 18 a 64 anni");
+    console.log(userTravelPrice);
+}
 
 document.getElementById("distance").innerHTML = userTravel;
 document.getElementById("age").innerHTML = userAge;
-
-var userTravelPrice = userTravel * distancePrice;
-console.log(userTravelPrice);
+document.getElementById("price").innerHTML = userTravelPrice;
